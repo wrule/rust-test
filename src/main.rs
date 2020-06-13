7c24a1e8d926c072c96a5538c62583c320712829
gu <1982775886@qq.com>
2020-06-14 00:01:22 +0800

    处理随机数

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+  println!("猜数字");
+
+  println!("请输入一个数字：");
+
+  let mut guess = String::new();
+
+  io::stdin().read_line(&mut guess)
+      .expect("失败，未能读输入");
+
+  println!("你输入的是: {}", guess);
+
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+  println!("{}", secret_number);
 }
