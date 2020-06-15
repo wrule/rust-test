04001bfb99f5d70c278c15cae000ed135e6ca731
鸡毛 <shihao.gu@perfma.com>
2020-06-15 09:35:48 +0800

    随机数修改

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
   println!("猜数字");
+
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+
+  println!("生成的随机数字是：{}", secret_number);
 
   println!("请输入一个数字：");
 
@@ -11,8 +16,8 @@ fn main() {
   io::stdin().read_line(&mut guess)
       .expect("失败，未能读输入");
 
-  println!("你输入的是: {}", guess);
+  let guess: u32 = guess.trim().parse()
+      .expect("请输入一个正确的数字");
 
-  let secret_number = rand::thread_rng().gen_range(1, 101);
-  println!("{}", secret_number);
+  println!("你输入的是: {}", guess);
 }
